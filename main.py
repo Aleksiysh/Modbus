677c932d9a21fb2687deff96954df02aca03b1e2
aleks-sh <aleks-sh@mail.ru>
2020-09-29 12:19:23 +0300

    100words

--- main.py
+++ main.py
@@ -1,32 +1,18 @@
-import modbus_tk
 import modbus_tk.defines as cst
 import modbus_tk.modbus_tcp as modbus_tcp
 import struct
 
 
 def to_float(arr):
-    ss = []
-    for i in range(0, len(arr), 4):
-        ss.append(arr[i + 2])
-        ss.append(arr[i + 3])
-        ss.append(arr[i + 0])
-        ss.append(arr[i + 1])
-
-    # ss = ([arr[d + 2], arr[d + 3], arr[d], arr[d + 1]]) for d in range(0, len(arr), 4)]
-
-    res = [(ss[d:d + 4]) for d in range(0, len(ss), 4)]
-
-    k = float_arr(res)
-    return k
-
-    pass
+    res = [[arr[d + 2], arr[d + 3], arr[d], arr[d + 1]] for d in range(0, len(arr), 4)]
+    return float_arr(res)
 
 
 def float_arr(arr):
     dd = []
     for t in arr:
         a, b, c, d = tuple(t)
-        k = struct.unpack("!f", a + b + c + d)
+        k, = struct.unpack("!f", a + b + c + d)
         dd.append(k)
     return dd
     pass
@@ -39,8 +25,7 @@ def main():
     slave = 1
     function_code = cst.READ_INPUT_REGISTERS
     starting_address = 0
-    quantity_of_x = 20
-    data_format = f"{quantity_of_x // 4}c"
+    quantity_of_x = 100
 
     getDI = master.execute(slave,
                            function_code,
@@ -49,7 +34,44 @@ def main():
                            data_format=f"{quantity_of_x * 2}c"
                            )
     f = to_float(getDI)
-    print(*f)
+    print(f)
+
+    f_100 = [17.269498825073242, 0.0, 18.0, 0.5, 50.0, 1.1020946175206465e-39, 0.0, 0.0, 0.0, 0.0, -50.0,
+             1.1020946175206465e-39, 0.0, 0.0, 0.0, 0.0, 0.0, 1.1020946175206465e-39, 0.0, 0.0, 0.0, 0.0, 0.0,
+             1.1020946175206465e-39, 0.0, 0.0, 0.0, 0.0, 0.0, 1.1020946175206465e-39, 0.0, 0.0, 0.0, 0.0, 0.0,
+             1.1020946175206465e-39, 0.0, 0.0, 0.0, 0.0, 0.0, 1.1020946175206465e-39, 1.8664011850778872e-14,
+             1.8661978971705462e-14, 0.0, 9.42324492633588e-15, 6.77377320901229e-10, 6.411114972237897e-10,
+             2.6175301075931356e-12, 0.002688419073820114]
+
+    qwe_100 = (
+        b"'", b'\xef', b'A', b'\x8a', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'A', b'\x90', b'\x00',
+        b'\x00',
+        b'?', b'\x00', b'\x00', b'\x00', b'B', b'H', b'\x00', b'1', b'\x00', b'\x0c', b'\x00', b'\x00', b'\x00',
+        b'\x00',
+        b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00',
+        b'\x00',
+        b'\x00', b'\xc2', b'H', b'\x00', b'1', b'\x00', b'\x0c', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00',
+        b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00',
+        b'\x00',
+        b'\x00', b'\x00', b'1', b'\x00', b'\x0c', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00',
+        b'\x00',
+        b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00',
+        b'\x00',
+        b'1', b'\x00', b'\x0c', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00',
+        b'\x00',
+        b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'1',
+        b'\x00',
+        b'\x0c', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00',
+        b'\x00',
+        b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'1', b'\x00', b'\x0c',
+        b'\x00',
+        b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00',
+        b'\x00',
+        b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'1', b'\x00', b'\x0c', b'\x1c', b'H', b'(',
+        b'\xa8',
+        b'\x17', b'\x98', b'(', b'\xa8', b'\x00', b'\x00', b'\x00', b'\x00', b'\xc1', b'\x0c', b'(', b')', b'2', b'1',
+        b'0',
+        b':', b':', b'6', b'0', b'0', b'1', b';', b',', b'8', b'0', b'0', b';', b'0')
 
     pass
 
