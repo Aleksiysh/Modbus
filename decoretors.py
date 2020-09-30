432cd9665282ae2ed9e9a17f8fb900c05aac44cf
aleks-sh <aleks-sh@mail.ru>
2020-09-30 10:40:37 +0300

    100words_1_refactor2

---
+++ decoretors.py
@@ -0,0 +1,21 @@
+def my_shiny_new_decorator(function_to_decorate):
+    '''
+    Внутри себя декоратор определяет функцию-"обёртку". Она будет обёрнута вокруг декорируемой,
+    получая возможность исполнять произвольный код до и после неё.
+    '''
+
+    def the_wrapper_around_the_original_function():
+        print("Я - код, который отработает до вызова функции")
+        function_to_decorate()  # Сама функция
+        print("А я - код, срабатывающий после")
+
+    # Вернём эту функцию
+    return the_wrapper_around_the_original_function
+
+
+def stand_alone_function():
+    print("Я простая одинокая функция, ты ведь не посмеешь меня изменять?")
+
+
+
+stand_alone_function()
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import struct
 
 
 def to_float(arr):
-    group_array = [list(arr[d:d+4]) for d in range(0, len(arr), 4)]
+    group_array = [tuple(arr[d:d + 4]) for d in range(0, len(arr), 4)]
     return float_arr(group_array)
 
 
@@ -13,7 +13,7 @@ def float_arr(arr):
     for t in arr:
         k, = struct.unpack("!f", t[2] + t[3] + t[0] + t[1])
         dd.append(k)
-    return dd
+    return tuple(dd)
 
 
 def main():
@@ -23,8 +23,8 @@ def main():
     slave = 1
     function_code = cst.READ_INPUT_REGISTERS
     starting_address = 0
-    quantity_of_x = 100
-
+    quantity_of_x = 84
+    #   while True:
     get_di = master.execute(slave,
                             function_code,
                             starting_address,
@@ -32,7 +32,8 @@ def main():
                             data_format=f"{quantity_of_x * 2}c"
                             )
     f = to_float(get_di)
-    print(f)
+    round_f = tuple(map(lambda a: round(a, 3), f))
+    #print(round_f)
 
     f_100 = [17.269498825073242, 0.0, 18.0, 0.5, 50.0, 1.1020946175206465e-39, 0.0, 0.0, 0.0, 0.0, -50.0,
              1.1020946175206465e-39, 0.0, 0.0, 0.0, 0.0, 0.0, 1.1020946175206465e-39, 0.0, 0.0, 0.0, 0.0, 0.0,
@@ -41,37 +42,38 @@ def main():
              1.8661978971705462e-14, 0.0, 9.42324492633588e-15, 6.77377320901229e-10, 6.411114972237897e-10,
              2.6175301075931356e-12, 0.002688419073820114]
 
-    qwe_100 = (
-        b"'", b'\xef', b'A', b'\x8a', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'A', b'\x90', b'\x00',
-        b'\x00',
-        b'?', b'\x00', b'\x00', b'\x00', b'B', b'H', b'\x00', b'1', b'\x00', b'\x0c', b'\x00', b'\x00', b'\x00',
-        b'\x00',
-        b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00',
-        b'\x00',
-        b'\x00', b'\xc2', b'H', b'\x00', b'1', b'\x00', b'\x0c', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00',
-        b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00',
-        b'\x00',
-        b'\x00', b'\x00', b'1', b'\x00', b'\x0c', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00',
-        b'\x00',
-        b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00',
-        b'\x00',
-        b'1', b'\x00', b'\x0c', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00',
-        b'\x00',
-        b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'1',
-        b'\x00',
-        b'\x0c', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00',
-        b'\x00',
-        b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'1', b'\x00', b'\x0c',
-        b'\x00',
-        b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00',
-        b'\x00',
-        b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'1', b'\x00', b'\x0c', b'\x1c', b'H', b'(',
-        b'\xa8',
-        b'\x17', b'\x98', b'(', b'\xa8', b'\x00', b'\x00', b'\x00', b'\x00', b'\xc1', b'\x0c', b'(', b')', b'2', b'1',
-        b'0',
-        b':', b':', b'6', b'0', b'0', b'1', b';', b',', b'8', b'0', b'0', b';', b'0')
+    ext = [(round_f[d:d + 5]) for d in range(0, len(round_f), 6)]
+    print(*ext,sep="\n")
 
-    pass
+    qwe_100 = (
+    b"'", b'\xef', b'A', b'\x8a', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'A', b'\x90', b'\x00',
+    b'\x00',
+    b'?', b'\x00', b'\x00', b'\x00', b'B', b'H', b'\x00', b'1', b'\x00', b'\x0c', b'\x00', b'\x00', b'\x00',
+    b'\x00',
+    b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00',
+    b'\x00',
+    b'\x00', b'\xc2', b'H', b'\x00', b'1', b'\x00', b'\x0c', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00',
+    b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00',
+    b'\x00',
+    b'\x00', b'\x00', b'1', b'\x00', b'\x0c', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00',
+    b'\x00',
+    b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00',
+    b'\x00',
+    b'1', b'\x00', b'\x0c', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00',
+    b'\x00',
+    b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'1',
+    b'\x00',
+    b'\x0c', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00',
+    b'\x00',
+    b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'1', b'\x00', b'\x0c',
+    b'\x00',
+    b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00',
+    b'\x00',
+    b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'1', b'\x00', b'\x0c', b'\x1c', b'H', b'(',
+    b'\xa8',
+    b'\x17', b'\x98', b'(', b'\xa8', b'\x00', b'\x00', b'\x00', b'\x00', b'\xc1', b'\x0c', b'(', b')', b'2', b'1',
+    b'0',
+    b':', b':', b'6', b'0', b'0', b'1', b';', b',', b'8', b'0', b'0', b';', b'0')
 
 
 if __name__ == '__main__':
